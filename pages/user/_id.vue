b069d73f460d3e86e46459203f92b9a9bb06382e
David Mosher <davidmosher@gmail.com>
2018-04-19 12:41:59 -0400

    semi-finished

---
+++ pages/user/_id.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{user.id}} {{user.created | timeSince }}
+  </div>
+</template>
+
+<script>
+  import axios from '~/plugins/axios'
+
+  export default {
+    async asyncData({route}) {
+      console.log('ROUTE PARAMS', route.params)
+      const res = await axios.get(`user/${route.params.id}`.json)
+      return { user: res.data }
+    }
+  }
+</script>
